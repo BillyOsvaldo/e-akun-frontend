26f357903ee94a454df5eb40f54f7b88b9a9d791
BillyOsvaldo <bill.jodh@gmail.com>
2018-04-10 11:36:42 +0700

    Add Organization Administrator Page and Update Administrator Page

---
+++ components/dialogs/manages/organizationadministrators/_add.vue
@@ -0,0 +1,212 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="dialogAddAdministratorsOrganizations" persistent scrollable max-width="600">
+      <v-card v-if="dialogAddAdministratorsOrganizations">
+        <v-card-title class="headline">Tambah Administrator Organisasi</v-card-title>
+        <v-card-text style="max-height: 300px;">
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-select
+                  autofocus
+                  label="Pilih Pengguna"
+                  autocomplete
+                  :loading="loading"
+                  v-bind:items="item_users"
+                  item-text="user"
+                  :search-input.sync="searchUser"
+                  v-validate="'required'"
+                  data-vv-name="user"
+                  :error-messages="errors.collect('user')"
+                  :cache-items="cacheItems"
+                  v-model="user"
+                ></v-select>
+              </v-flex>
+               <v-flex xs12>
+                <v-select
+                  v-model="permissions"
+                  label="Permissions"
+                  multiple
+                  chips
+                  tags
+                  :items="itemsPermissions">
+                ></v-select>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat @click.native="closeDialogButton">Batal</v-btn>
+          <v-btn flat color="blue darken-1"
+            @click.native="postUpdate">Tambah</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+import {mapState, mapGetters} from 'vuex'
+const customHelptext = {
+  en: {
+    custom: {
+      user: {
+        required: 'Nama harus di isi'
+      },
+      permission: {
+        required: 'Permissions harus di isi'
+      }
+    }
+  }
+}
+export default {
+  data () {
+    return {
+      dialogAddAdministratorsOrganizations: false,
+      cacheItems: true,
+      user: null,
+      searchUser: null,
+      loading: false,
+      search: null,
+      tempItems: [],
+      tempItemsApp: [],
+      permissions: [],
+      administrator: null,
+      id: null,
+      userSelected: null
+    }
+  },
+  computed: {
+    ...mapState({
+      administratorsorganization: 'administratorsorganizationsmanagement'
+    }),
+    ...mapGetters({
+      adminorganizationList: 'administratorsorganizationsmanagement/current',
+      usersselect: 'usersselect/list',
+      organizationusersList: 'organizationusersdraftmanagement/list',
+      permissionsselect: 'permissionsadminorganizationsselect/list'
+    }),
+    item_users () {
+      let _output = []
+      if (this.usersselect.length > 0) {
+        this.usersselect.forEach((item) => {
+          if (typeof this.organizationusersList.find((i) => i.user._id === item._id) !== 'object') {
+            let data = {
+              _id: item._id,
+              user: ((item.profile.name.first_title === '' ? '' : item.profile.name.first_title + '.')) + item.profile.name.first_name + ' ' + item.profile.name.last_name + ((item.profile.name.last_title === '' ? '' : ', ' + item.profile.name.last_title)) + (item.profile.nip ? ' - ' + item.profile.nip : '')
+            }
+            _output.push(data)
+            this.tempItems.push(item)
+          }
+        })
+      }
+      return _output
+    },
+    itemsPermissions () {
+      let _output = []
+      if (this.permissionsselect.length > 0) {
+        this.permissionsselect.forEach((permission) => {
+          _output.push(permission.app.name)
+        })
+      }
+      return _output
+    },
+    permissionList () {
+      let output = []
+      this.permissions.forEach((name) => {
+        let permission = this.permissionsselect.find((item) => item.app.name === name)
+        output.push(permission._id)
+      })
+      return output
+    }
+  },
+  watch: {
+    searchUser (val) {
+      this.cacheItems = false
+      this.$store.commit('usersselect/clearAll')
+      if (this.user !== null && typeof this.user === 'object') {
+        let item = this.tempItems.find((item) => item._id === this.user._id)
+        if (item !== undefined) {
+          let value = ((item.profile.name.first_title === '' ? '' : item.profile.name.first_title + '.')) + item.profile.name.first_name + ' ' + item.profile.name.last_name + ((item.profile.name.last_title === '' ? '' : ', ' + item.profile.name.last_title)) + (item.profile.nip ? ' - ' + item.profile.nip : '')
+          let _data = {
+            _id: item._id,
+            user: value
+          }
+          this.user = _data
+          this.item_users.push(_data)
+        }
+      }
+      if (val && val.length >= 3) {
+        let params = {
+          query: {
+            $first_name_or_last_name: val
+          }
+        }
+        this.$store.dispatch('usersselect/find', params)
+      }
+    },
+    user (val) {
+      if (val) {
+        this.userOnChange(val)
+      }
+    }
+  },
+  methods: {
+    userOnChange (val) {
+      if (val) {
+        let select = this.usersselect.find((item) => item._id === val._id)
+        if (select && select.permissions) {
+          this.permissions = []
+          select.permissions.forEach((item) => {
+            this.permissions.push(item.app.name)
+          })
+        }
+      }
+    },
+    onBlur (field) {
+      setTimeout(() => this.$validator.validate(field), 500)
+    },
+    closeDialogButton () {
+      this.dialogAddAdministratorsOrganizations = !this.dialogAddAdministratorsOrganizations
+      this.resetAll()
+    },
+    postUpdate () {
+      this.$validator.validateAll()
+        .then((result) => {
+          if (result) {
+            let data = {
+              permissions: this.permissionList
+            }
+            let params = {}
+            this.$store.commit('administratorsorganizationsmanagement/clearCreateError')
+            this.$store.dispatch('administratorsorganizationsmanagement/patch', [this.user._id, data, params])
+              .then(response => {
+                if (response) {
+                  this.dialogAddAdministratorsOrganizations = false
+                  this.resetAll()
+                }
+              })
+          }
+        })
+    },
+    resetAll () {
+      this.$store.commit('administratorsorganizationsmanagement/clearCreateError')
+      this.$validator.reset()
+      this.name = null
+      this.app = null
+      this.administrator = null
+      this.user = null
+      this.permissions = []
+    }
+  },
+  created () {
+    this.$root.$on('opendialogAddAdministratorsOrganizations', () => {
+      this.dialogAddAdministratorsOrganizations = true
+    })
+    this.$validator.localize(customHelptext)
+  }
+}
+</script>
+<style lang="sass">
+</style>
---
+++ pages/manages/organizationadministrators.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="administratorsorganization-content" v-resize="onResize" style="background: #fff;">
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      hide-actions
+      class="administratorsorganization"
+      id="scroll-target"
+      :pagination.sync="pagination"
+      v-bind="loadData + loadNextPage"
+    >
+      <template slot="items" slot-scope="props">
+        <td style="font-weight: 500;">{{ props.item.profile.nip }}</td>
+        <td style="font-weight: 500;">{{ props.item.profile.name.first_name + ' ' + props.item.profile.name.last_name }}</td>
+        <td style="font-weight: 500;">{{ props.item.username }}</td>
+        <td class="text-xs-left">{{ props.item.email }}</td>
+         <td class="text-xs-center">
+          <div>
+            <v-tooltip
+              top
+              >
+              <v-btn
+                slot="activator" icon class="mx-0" @click.native="EditItem(props.item)">
+                <v-icon color="grey darken-1">edit</v-icon>
+              </v-btn>
+              <span>Ubah Data</span>
+            </v-tooltip>
+          </div>
+        </td>
+      </template>
+      <template slot="no-data">
+        <span>Belum ada data.</span>
+      </template>
+    </v-data-table>
+    <v-fab-transition>
+      <v-btn
+        color="red"
+        dark
+        fixed
+        bottom
+        right
+        fab
+        @click.native="$root.$emit('opendialogAddAdministratorsOrganizations')"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+    </v-fab-transition>
+    <dialogAdd/>
+    <dialogEdit/>
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters} from 'vuex'
+import dialogAdd from '~/components/dialogs/manages/organizationadministrators/_add'
+import dialogEdit from '~/components/dialogs/manages/organizationadministrators/_edit'
+import {generateTable, resizeTable, loadData} from '~/utils/datatable'
+export default {
+  data: () => ({
+    dialog: false,
+    tableCreated: false,
+    scrollBottom: false,
+    nextPage: false,
+    sortValue: {},
+    skipPage: 0,
+    windowSize: {
+      x: 0,
+      y: 0
+    },
+    headers: [
+      { text: 'NIP', align: 'left', value: 'nip' },
+      { text: 'Nama', align: 'left', value: 'name' },
+      { text: 'Username', align: 'left', value: 'username' },
+      { text: 'Email', value: 'email', sortable: false, align: 'left' },
+      { text: '', align: 'left', value: '' },
+      { text: '', value: 'name', sortable: false, class: 'action' }
+    ],
+    pagination: {
+      sortBy: 'name',
+      rowsPerPage: -1,
+      descending: true
+    },
+    items: [],
+    doResendEmail: false,
+    snackbarView: false,
+    textSnackbar: ''
+  }),
+  components: {
+    dialogAdd,
+    dialogEdit
+  },
+  computed: {
+    ...mapState({
+      administratorsorganization: 'administratorsorganizationsmanagement'
+    }),
+    ...mapGetters({
+      adminorganizationList: 'administratorsorganizationsmanagement/list'
+    }),
+    loadData () {
+      if (typeof this.adminorganizationList !== 'undefined') {
+        this.items = this.adminorganizationList
+        if (this.items.length > 0 && this.tableCreated) {
+          loadData(this, 'administratorsorganization', this.items.length)
+        }
+      }
+    },
+    loadNextPage () {
+      this.getNextPage()
+    }
+  },
+  created () {
+    this.$store.commit('administratorsorganizationsmanagement/clearAll')
+    this.initialize()
+  },
+  watch: {
+    pagination: {
+      handler (val) {
+        if (val.sortBy !== null) {
+          let sortBy = val.sortBy.replace('_timestamp', '')
+          this.sortValue = {
+            [sortBy]: (val.descending === true) ? -1 : 1
+          }
+          let params = {
+            query: {
+              $sort: this.sortValue
+            }
+          }
+          this.$store.dispatch('administratorsorganizationsmanagement/find', params)
+        }
+      }
+    },
+    deep: true
+  },
+  methods: {
+    onResize () {
+      resizeTable(this, window, 'administratorsorganization')
+    },
+    initialize () {
+      let params = {
+        query: {}
+      }
+      this.$store.dispatch('administratorsorganizationsmanagement/find', params)
+      let paramsforDialogs = {
+        query: {}
+      }
+      this.$store.dispatch('appsselect/find', paramsforDialogs)
+      this.$store.dispatch('permissionsadminorganizationsselect/find', paramsforDialogs)
+      this.$store.dispatch('administratorsselect/find', paramsforDialogs)
+    },
+    getNextPage () {
+      if (!this.scrollBottom) {
+        this.nextPage = false
+      }
+
+      if (this.scrollBottom && !this.nextPage && this.items.length < this.administratorsorganization.pagination.default.total) {
+        this.nextPage = true
+        this.skipPage++
+        let skipValue = this.skipPage * 10
+        if (skipValue > this.administratorsorganization.pagination.default.total) {
+          skipValue = this.administratorsorganization.pagination.default.total
+        }
+        let params = {
+          query: {
+            $sort: this.sortValue,
+            $skip: skipValue
+          }
+        }
+        this.$store.dispatch('administratorsorganizationsmanagement/find', params)
+      }
+    },
+    EditItem (item) {
+      this.$store.commit('administratorsorganizationsmanagement/setCurrent', item)
+      this.$root.$emit('openDialogEditAdministratorOrganization')
+    }
+  },
+  mounted () {
+    this.$store.dispatch('setNavigationTitle', 'Manajemen Administrator Organization')
+    generateTable(this, window, 'administratorsorganization')
+  }
+}
+</script>
+
+<style lang="sass">
+  .administratorsorganization
+    position: relative
+    zoom: 1
+    min-width: 1000px
+    min-height: 20px
+    display: block
+    width: 100%
+    overflow: unset
+</style>
